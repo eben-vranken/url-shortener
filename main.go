dcdc78ab0fcb0f22c8a86eac6fa84374267715a9
eben-vranken <vranken.eben@outlook.com>
2026-05-17 15:09:49 +0200

    feat: implemented read-write mutex with better error handling

--- main.go
+++ main.go
@@ -3,51 +3,130 @@ package main
 import (
 	"crypto/rand"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
+	"sync"
 )
 
 var urls map[string]string = make(map[string]string)
 
-var reverse_urls map[string]string = make(map[string]string)
+var mu sync.RWMutex
 
-type UrlStruct struct {
-	Url string
+var reverseURLs map[string]string = make(map[string]string)
+
+type URLStruct struct {
+	URL string
 }
 
 func main() {
-	http.HandleFunc("/new", generateShortened)
+	http.HandleFunc("POST /new", generateSlug)
 	http.HandleFunc("/{url}", redirectFromShortened)
 
-	http.ListenAndServe(":3000", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func generateShortened(w http.ResponseWriter, req *http.Request) {
+func generateSlug(w http.ResponseWriter, req *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
 	decoder := json.NewDecoder(req.Body)
-	var response UrlStruct
+	var response URLStruct
 	err := decoder.Decode(&response)
 
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	if reverse_urls[response.Url] == "" {
-		slug := rand.Text()[:5]
-		urls[slug] = response.Url
-		reverse_urls[response.Url] = slug
-	}
-
-	fmt.Fprint(w, reverse_urls[response.Url])
-}
-
-func redirectFromShortened(w http.ResponseWriter, req *http.Request) {
-	fullUrl := urls[req.URL.Path[1:]]
-
-	if fullUrl == "" {
-		fmt.Fprint(w, "404 - URL does not exists")
+		statusCode := http.StatusInternalServerError
+		msg := "500 - Interal server error"
+		http.Error(w, msg, statusCode)
 		return
 	}
 
-	http.Redirect(w, req, fullUrl, http.StatusTemporaryRedirect)
+	ok, normalized := isValidURL(response.URL)
+
+	if !ok {
+		statusCode := http.StatusBadRequest
+		msg := "400 - Not a valid URL"
+		http.Error(w, msg, statusCode)
+		return
+	}
+
+	response.URL = normalized
+
+	if reverseURLs[response.URL] == "" {
+		slug := rand.Text()[:5]
+
+		for urls[slug] != "" {
+			slug = rand.Text()[:5]
+		}
+
+		urls[slug] = response.URL
+		reverseURLs[response.URL] = slug
+	}
+
+	url := URLStruct{URL: reverseURLs[response.URL]}
+
+	j, err := json.Marshal(url)
+
+	if err != nil {
+		statusCode := http.StatusInternalServerError
+		msg := "500 - Interal server error"
+		http.Error(w, msg, statusCode)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func redirectFromShortened(w http.ResponseWriter, req *http.Request) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	fullURL := urls[req.PathValue("url")]
+
+	if fullURL == "" {
+		statusCode := http.StatusNotFound
+		msg := "404 - URL does not exist."
+		http.Error(w, msg, statusCode)
+		return
+	}
+
+	http.Redirect(w, req, fullURL, http.StatusTemporaryRedirect)
+}
+
+func isValidURL(urlToParse string) (bool, string) {
+	if urlToParse == "" {
+		return false, ""
+	}
+
+	u, err := url.Parse(urlToParse)
+
+	if err != nil {
+		return false, ""
+	}
+
+	if !strings.Contains(u.Host, ".") {
+		return false, ""
+	}
+
+	if u.Scheme == "" {
+		urlToParse = "https://" + urlToParse
+		u, err = url.Parse(urlToParse)
+
+		if err != nil {
+			return false, ""
+		}
+	}
+
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return false, ""
+	}
+
+	if u.Host == "" {
+		return false, ""
+	}
+
+	return true, urlToParse
 }
